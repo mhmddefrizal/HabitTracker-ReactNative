5293a2d7a4567bddd527ed9c33e1077c7db2d34a
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-24 00:46:42 +0700

    import link halaman login di root index.tsx

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Lorem ipsum dolor sit amet.</Text>
+      <Link href="/login">Halaman Login</Link>
     </View>
   );
 }
