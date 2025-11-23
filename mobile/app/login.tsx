a50fc12fe25bef115abf199dfb2ed7dae35b0518
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-24 00:38:57 +0700

    tahap awal buat halaman login

---
+++ mobile/app/login.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View>
+      {" "}
+      <Text>Ini adalah halaman login</Text>
+    </View>
+  );
+}
