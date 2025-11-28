740a8edb99fbba88e24870fd55aa6a37f6a2eaf2
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-28 15:42:19 +0700

    tambahkan ! di belakang masing-masing variabelnya agar tidak error

--- mobile/lib/appwrite.ts
+++ mobile/lib/appwrite.ts
@@ -1,3 +1,3 @@
 import { Client } from "react-native-appwrite";
 
-const client = new Client().setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT).setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID).setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM);
+const client = new Client().setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!).setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!).setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
