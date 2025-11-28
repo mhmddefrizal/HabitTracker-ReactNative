314be12948b44bbf5379964a85a778363762fd0d
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-28 21:14:38 +0700

    feat: menambahkan redirect ke halaman auth pada RouteGuard

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,19 +1,24 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { useEffect } from "react";
 
 function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   const isAuth = false;
 
   useEffect(() => {
     if (!isAuth) {
-      // redirect to login screen
+      router.replace("/auth");
     }
   });
+
+  return <>{children}</>;
 }
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <RouteGuard>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </RouteGuard>
   );
 }
