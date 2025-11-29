f8d697ce9b516c3a234c306443b068d779ffbfa2
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-30 00:46:42 +0700

    feat: menambahkan logika toggle isSignUp pada AuthScreen

--- mobile/app/auth.tsx
+++ mobile/app/auth.tsx
@@ -1,19 +1,22 @@
+import { useState } from "react";
 import { KeyboardAvoidingView, Platform, View } from "react-native";
 
 import { Button, Text, TextInput } from "react-native-paper";
 
 export default function AuthScreen() {
+  // buat useState Sign Up
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
   return (
     <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
       <View>
-        <Text>Buat Akun</Text>
+        <Text>{isSignUp ? "Buat Akun" : "Selamat datang kembali"}</Text>
 
         <TextInput label="Email" autoCapitalize="none" keyboardType="email-address" placeholder="example@gmail.com" mode="outlined" />
 
         <TextInput label="Password" autoCapitalize="none" keyboardType="email-address" mode="outlined" />
 
-        <Button mode="contained">Daftar Akun</Button>
-        <Button mode="text">Sudah punya akun? Masuk</Button>
+        <Button mode="contained">{isSignUp ? "Daftar Akun" : "Masuk"}</Button>
+        <Button mode="text">{isSignUp ? "Sudah punya akun? Masuk" : "Belum punya akun? Daftar"}</Button>
       </View>
     </KeyboardAvoidingView>
   );
