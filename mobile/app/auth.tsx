652e63ae87e71c765c143e3de0fa767065aa018d
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-29 20:54:23 +0700

    tambah text input email di auth.tsx

--- mobile/app/auth.tsx
+++ mobile/app/auth.tsx
@@ -1,10 +1,12 @@
-import { KeyboardAvoidingView, Platform, Text, View } from "react-native";
+import { KeyboardAvoidingView, Platform, Text, TextInput, View } from "react-native";
 
 export default function AuthScreen() {
   return (
     <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
       <View>
         <Text>Buat Akun</Text>
+
+        <TextInput label="Email" autoCapitalize="none" keyboardType="email-address" placeholder="example@gmail.com" />
       </View>
     </KeyboardAvoidingView>
   );
