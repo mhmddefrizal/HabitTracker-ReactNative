4716ca92aa4006b2da58e66880467f0ff76f2bcc
okawindanasaputra <okwan2004@gmail.com>
2025-12-01 12:38:07 +0700

    menambahkan import auth provider di layout app

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "@/lib/auth-context";
 import { Stack, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
 
@@ -21,10 +22,12 @@ function RouteGuard({ children }: { children: React.ReactNode }) {
 
 export default function RootLayout() {
   return (
+    <AuthProvider>
     <RouteGuard>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </RouteGuard>
+    </AuthProvider>
   );
 }
