00e54a346fe0da4469fa6a1712856416643b2f89
okawindanasaputra <okwan2004@gmail.com>
2025-12-01 12:43:22 +0700

    menambah fungsi error handling pada useAuth

--- mobile/lib/auth-context.tsx
+++ mobile/lib/auth-context.tsx
@@ -46,4 +46,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
 export function useAuth() {
     const context = useContext(AuthContext);
+    
+if (context === undefined) {
+        throw new Error("useAuth harus berada di dalam AuthProvider");
+    }
 }
