3d2ba95a9168552392f8a1e2d9e7cfe14fd0ef18
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-12-17 00:42:12 +0700

    feat: menambahkan redirect berdasarkan status login user

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,17 +1,24 @@
 import { AuthProvider, useAuth } from "@/lib/auth-context";
-import { Stack, useRouter } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { useEffect, useState } from "react";
 
 function RouteGuard({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-
   // buat state user dari useAuth agar bisa digunakan di komponen lain
   const { user } = useAuth();
 
+  // buat state segments dari useSegments agar bisa digunakan di komponen lain
+  const segments = useSegments();
+
   // buat useEffect ketika user null maka redirect ke halaman /auth
   useEffect(() => {
-    if (!user) {
+    // cek apakah route berada di group auth
+    const inAuthGroup = segments[0] === "auth";
+    // jika user null dan tidak berada di group auth maka redirect ke halaman /auth
+    if (!user && !inAuthGroup) {
       router.replace("/auth");
+    } else if (user && inAuthGroup) {
+      router.replace("/");
     }
   });
 
