61d5719c59df513251a2da2e6de66424b25cbc0d
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-12-18 13:52:32 +0700

    feat: menambahkan konfigurasi tab navigator dan icon tab

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,20 +1,23 @@
+import MaterialCommunityIcons from "@expo/vector-icons/AntDesign";
 import { Tabs } from "expo-router";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
-import AntDesign from "@expo/vector-icons/AntDesign";
 import Entypo from "@expo/vector-icons/Entypo";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerStyle: { backgroundColor: "#f5f5f5" }, headerShadowVisible: false, tabBarStyle: { backgroundColor: "#f5f5f5", borderTopWidth: 0, elevation: 0, shadowOpacity: 0 } }}>
+    <Tabs
+      screenOptions={{
+        headerStyle: { backgroundColor: "#f5f5f5" },
+        headerShadowVisible: false,
+        tabBarStyle: { backgroundColor: "#f5f5f5", borderTopWidth: 0, elevation: 0, shadowOpacity: 0 },
+        tabBarActiveTintColor: "#6200ee",
+        tabBarInactiveTintColor: "#666",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
-          tabBarIcon: ({ color, focused }) => {
-            {
-              return focused ? <FontAwesome5 name="home" size={24} color={color} /> : <AntDesign name="home" size={24} color="black" />;
-            }
-          },
+          title: "Today's Habits",
+          tabBarIcon: ({ color, focused }) => <MaterialCommunityIcons />,
         }}
       />
       <Tabs.Screen
