692812d51fbdfd82339df22157fbf14a6a7ba600
Rivaldzz <rivaldosborn@gmail.com>
2025-12-20 21:41:45 +0700

    Penambahan const style cardDescription

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -92,6 +92,11 @@ const styles = StyleSheet.create({
     marginBottom: 4,
     color: "#22223b",
   },
+  cardDescription: {
+    fontSize: 16,
+    marginBottom: 15,
+    color: "#6c6c80",
+  }
 
   navButton: {
     width: 100,
