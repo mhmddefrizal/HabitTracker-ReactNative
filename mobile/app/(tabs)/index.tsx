520f757fdbc9dbc3157a6c5ed07050f5122ff548
Rivaldzz <rivaldosborn@gmail.com>
2025-12-20 21:10:02 +0700

    Tambah fungsi dalam UI aplikasi

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,21 +1,30 @@
 import { useAuth } from "@/lib/auth-context";
 import { Link } from "expo-router";
-import { Text,View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
+import TambahHabitScreen from "./tambah-habit";
 
 export default function Index() {
   // buat variabel signOut dari useAuth
   const { signOut } = useAuth();
   return (
     <View style={styles.view}>
-      {/* <Text>Lorem ipsum dolor sit amet.</Text>
-      <Button mode="text" onPress={signOut} icon={"logout"}>
-        {" "}
-        Keluar{" "}
-      </Button> */}
       <View>
         <Text>Agenda Hari Ini</Text>
+        <Text>Lorem ipsum dolor sit amet.</Text>
+        <Button mode="text" onPress={signOut} icon={"logout"}>
+          {" "}
+          Keluar{" "}
+        </Button>
       </View>
+
+      {TambahHabitScreen?.length === 0 ? (
+        <View>
+          <Text>Tidak ada agenda hari ini, tambah agenda pertamamu!</Text>
+        </View>
+      ): (
+        TambahHabitScreen.map((habit, key) => <View> </View>
+      )}
     </View>
   );
 }
