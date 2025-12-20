e992337b318c913d6296a0cd99e6f68dfc9b2cc4
Rivaldzz <rivaldosborn@gmail.com>
2025-12-20 23:10:00 +0700

    penambahan swipeable dan variabel swipeableref

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -6,12 +6,15 @@ import TambahHabitScreen from "./tambah-habit";
 import CardTitle from "react-native-paper/lib/typescript/components/Card/CardTitle";
 import { useEffect } from "react";
 import { client } from "@/lib/appwrite";
+import { Swipeable } from "react-native-gesture-handler";
 
 export default function Index() {
   // buat variabel signOut dari useAuth
   const { signOut, user } = useAuth();
 
   const [habits, setHabits] = useState<Habit[]>();
+
+  const swipeableRef = useRef<{[key: string]: Swipeable | null}>({});
 
   useEffect(() => {
     if (user) {
@@ -54,29 +57,33 @@ export default function Index() {
           </View>
         ) : (
           habits.map((habit, key) => (
-            <Surface style={styles.card} elevation={0}>
-              <View key={key} style={styles.cardContent}>
-                <Text style={styles.cardTitle}>
-                  {habit.title}
-                </Text>
-                <Text style={styles.cardDescription}>
-                  {habit.description}
-                </Text>
-                <View style={styles.cardFooter}>
-                  <View style={styles.streakBadge}>
-                    <MaterialCommunityIcon name="fire" size={18} color="#ff9800 ">
+            <Swipeable ref={(ref)=>{
+              swipeableRef.current{habit.$id}=ref
+            }}>
+              <Surface style={styles.card} elevation={0}>
+                <View key={key} style={styles.cardContent}>
+                  <Text style={styles.cardTitle}>
+                    {habit.title}
+                  </Text>
+                  <Text style={styles.cardDescription}>
+                    {habit.description}
+                  </Text>
+                  <View style={styles.cardFooter}>
+                    <View style={styles.streakBadge}>
+                      <MaterialCommunityIcon name="fire" size={18} color="#ff9800 ">
 
-                    </MaterialCommunityIcon>
-                    <Text style={styles.streakText}>
-                      {habit.streak_count} rentetan hari
-                    </Text>
-                  </View>
-                  <View style={styles.frequencyBadge}>
-                    <Text style={styles.frequencyText}>{habit.frequency.charAt(0).toUpperCase() + habit.frequency.slice(1)}</Text>
+                      </MaterialCommunityIcon>
+                      <Text style={styles.streakText}>
+                        {habit.streak_count} rentetan hari
+                      </Text>
+                    </View>
+                    <View style={styles.frequencyBadge}>
+                      <Text style={styles.frequencyText}>{habit.frequency.charAt(0).toUpperCase() + habit.frequency.slice(1)}</Text>
+                    </View>
                   </View>
                 </View>
-              </View>
-            </Surface>
+              </Surface>
+            </Swipeable>
           ))
         )}
       </ScrollView>
@@ -84,94 +91,94 @@ export default function Index() {
   );
 }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 16,
-      backgroundColor: "#f5f5f5",
-      // justifyContent: "center",
-      // alignItems: "center",
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+  },
+  title: {
+    fontWeight: "bold",
+  },
+  card: {
+    marginBottom: 18,
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    header: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginBottom: 24,
-    },
-    title: {
-      fontWeight: "bold",
-    },
-    card: {
-      marginBottom: 18,
-      borderRadius: 8,
-      backgroundColor: "#fff",
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-    },
-    cardContent: {
-      padding: 22,
-    },
-    cardTitle: {
-      fontSize: 20,
-      fontWeight: "bold",
-      marginBottom: 4,
-      color: "#22223b",
-    },
-    cardDescription: {
-      fontSize: 16,
-      marginBottom: 15,
-      color: "#6c6c80",
-    },
-    cardFooter: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-    },
-    streakBadge: {
-      flexDirection: "row",
-      alignItems: "center",
-      backgroundColor: "#ff9800",
-      borderRadius: 12,
-      paddingHorizontal: 10,
-      paddingVertical: 4,
-    },
-    streakText: {
-      marginLeft: 8,
-      color: "#fff9800",
-      fontWeight: "bold",
-      fontSize: 14,
-    },
-    frequencyBadge: {
-      backgroundColor: "#2196f3",
-      borderRadius: 12,
-      paddingHorizontal: 12,
-      paddingVertical: 4,
-    },
-    frequencyText: {
-      marginLeft: 8,
-      color: "#7c4dff",
-      fontWeight: "bold",
-      fontSize: 14,
-    },
-    emptyState: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    emptyStateText: {
-      color: "#a51c31",
-    },
-    navButton: {
-      width: 100,
-      height: 20,
-      backgroundColor: "coral",
-      borderRadius: 8,
-      textAlign: "center",
-    },
-  });
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  cardContent: {
+    padding: 22,
+  },
+  cardTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: "#22223b",
+  },
+  cardDescription: {
+    fontSize: 16,
+    marginBottom: 15,
+    color: "#6c6c80",
+  },
+  cardFooter: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  streakBadge: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#ff9800",
+    borderRadius: 12,
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+  },
+  streakText: {
+    marginLeft: 8,
+    color: "#fff9800",
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  frequencyBadge: {
+    backgroundColor: "#2196f3",
+    borderRadius: 12,
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+  },
+  frequencyText: {
+    marginLeft: 8,
+    color: "#7c4dff",
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  emptyState: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  emptyStateText: {
+    color: "#a51c31",
+  },
+  navButton: {
+    width: 100,
+    height: 20,
+    backgroundColor: "coral",
+    borderRadius: 8,
+    textAlign: "center",
+  },
+});
