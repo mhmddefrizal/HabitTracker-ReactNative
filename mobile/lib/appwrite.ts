08f6159a38e0db523db222bfa02239e2bb97a153
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-01-07 22:46:45 +0700

    feat: menambahkan konfigurasi Databases Appwrite dan environment ID

--- mobile/lib/appwrite.ts
+++ mobile/lib/appwrite.ts
@@ -1,5 +1,9 @@
-import { Account, Client } from "react-native-appwrite";
+import { Account, Client, Databases } from "react-native-appwrite";
 
 export const client = new Client().setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!).setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!).setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
 
 export const account = new Account(client);
+export const databases = new Databases(client);
+
+export const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DB_ID!;
+export const HABITS_TABLE_ID = process.env.EXPO_PUBLIC_HABITS_TABLE_ID!;
