c7359fd1f3e394ff2875870e496e9f72c8c88052
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-01-07 22:59:55 +0700

    feat: menambahkan form input dan pilihan frekuensi habit

--- mobile/app/(tabs)/tambah-habit.tsx
+++ mobile/app/(tabs)/tambah-habit.tsx
@@ -1,9 +1,20 @@
 import { Text, View } from "react-native";
+import { SegmentedButtons, TextInput } from "react-native-paper";
 
+const FREQUENCIES = ["harian", "mingguan", "bulanan"];
 export default function TambahHabitScreen() {
   return (
     <View>
-      <Text>Ini adalah halaman login</Text>
+      <TextInput label="Title" mode="outlined" />
+      <TextInput label="Description" mode="outlined" />
+      <View>
+        <SegmentedButtons
+          buttons={FREQUENCIES.map((freq) => ({
+            value: freq,
+            label: freq.charAt(0).toUpperCase() + freq.slice(1),
+          }))}
+        />
+      </View>
     </View>
   );
 }
